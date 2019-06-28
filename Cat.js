a83875a8e50878d4c108c0667a5957b44343d802
Khoa Bui <khoabuivnhcm@gmail.com>
2019-06-28 15:19:23 +0700

    Add property isDead to Cat

--- Cat.js
+++ Cat.js
@@ -1,9 +1,12 @@
 function Cat(name)  {
   this.name = name;
   this.stomach = [];
-
+  this.isDead = false;
 }
 Cat.prototype.eat = function(mouse) {
   this.stomach.push(mouse);
 }
+Cat.prototype.die = function() {
+  this.isDead = true;
+};
 module.exports = Cat;
